0a12333bb464b7faf1cefc75a8b8abf24c7ceeed
Tech-A60 <tech-a60@Tech-A60s-Air.home>
2018-05-29 21:46:40 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// 12. Write a function called 'fizzBuzz' that takes any number and returns a value based on these rules
+// function fizzBuzz (num) {
+//   if (num % 3 === 0 && num % 5 === 0) {
+//     return num = `FizzBuzz`;
+//   } else if (num % 5 === 0) {
+//     return num = `Buzz`;
+//   } else if (num % 3 === 0) {
+//     return num = `Fizz`;
+//   } else {
+//     return num;
+//   }
+// }
+// But for multiples of three print "Fizz" instead of the number
+// For the multiples of five print "Buzz".
+// For numbers which are multiples of both three and five print "FizzBuzz".
